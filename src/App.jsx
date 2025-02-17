37ebacb7560650129ad2853f9ca38a8aa5c0cd74
Mouhssine Kinani <kinanimouhssine@gmail.com>
2025-02-17 09:40:04 +0100

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 
 import './App.css'
-// import Form from './SSv1/Form'
-// import SalaryCalculator from './SSv1/SSv2'
 import Test3 from './SSv1/SSV3'
 function App() {
 
   return (
    <>
-      {/* <Form/>
-      <SalaryCalculator/> */}
       <Test3/>
    </>
   )
